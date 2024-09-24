f207ddc7ebef2efe129a1a7f5b37f4b61503b7a3
BewJSDK <bewjsdk41@gmail.com>
2024-09-24 12:06:31 +0700

    create navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,37 +1,48 @@
+import { useState } from "react";
 import styles from "./navbar.module.css";
 import { IoFastFood, IoSearch, IoBasket } from "react-icons/io5";
-
+import { navbarMenu } from "../../constants";
 
 const Navbar = () => {
+  // Current menu path
+  const [menuPath, setMenuPath] = useState("home");
+
   return (
     <nav className={styles.navbar}> 
       {/* Food delivery Logo */}
       <div className={styles.logo}>
         <a href="#">
-          <IoFastFood size={32} />
+          <span>
+            <IoFastFood size={40} />
+          </span>
           <span>JSDKFOOD</span>
         </a>
       </div>
 
       {/* Navbar Menu */}
       <ul className={styles.navMenu}>
-        <li>home</li>
-        <li>menu</li>
-        <li>mobile-app</li>
-        <li>contact us</li>
+        {/* Menu lists */}
+        {navbarMenu.map(menu => (
+          <li 
+            key={menu.label}
+            className={menu.label === menuPath ? styles.active : ""}
+          >
+            <button onClick={() => setMenuPath(menu.label)}>{menu.label}</button>
+          </li>
+        ))}
       </ul>
 
       {/* Navbar right side */}
       <div className={styles.navRight}>
         {/* Search icon */}
         <button>
-          <IoSearch size={24} />
+          <IoSearch size={32} />
         </button>
         {/* Basket icon */}
-        <div className={styles.basket}>
-          <IoBasket size={24} />
+        <button className={styles.basket}>
+          <IoBasket size={32} />
           <div className={styles.basketNoti}></div>
-        </div>
+        </button>
         {/* Sign in */}
         <button>Sign in</button>
       </div>
---
+++ src/constants/index.ts
@@ -0,0 +1,18 @@
+export const navbarMenu = [
+  {
+    label: 'home',
+    path: '/',
+  },
+  {
+    label: 'menu',
+    path: '/menu',
+  },
+  {
+    label:'mobile-app',
+    path: '/mobile-app',
+  },
+  {
+    label: 'contact us',
+    path: '/contact-us',
+  }
+];
