c4b9e48812ab28a6f5936b98018c80a633f07575
BewJSDK <bewjsdk41@gmail.com>
2024-09-26 08:02:16 +0700

    create FoodDisplay Section

---
+++ src/components/FoodDisplay/FoodDisplay.tsx
@@ -0,0 +1,35 @@
+import { useContext } from "react";
+import styles from "./foodDisplay.module.css";
+import { StoreContext } from "../../context/StoreContext";
+import { FoodType } from "../../interfaces";
+import FoodItem from "../FoodItem/FoodItem";
+
+interface FoodDisplayProps {
+  category: FoodType
+}
+
+const FoodDisplay = ({ category }: FoodDisplayProps) => {
+
+  const context = useContext(StoreContext);
+  
+  if(!context) return <div>Loading...</div>
+
+  const { foodLists } = context;
+  
+  return (
+    <section className={styles.foodDisplay} id="foodDisplay">
+      <h2>Top dishes near you</h2>
+      {/* Food Lists */}
+      <div className={styles.foodDisplayList}>
+        {foodLists.map(item => (
+          <FoodItem 
+            key={item.id}
+            foodData={item}
+          />
+        ))}
+      </div>
+    </section>
+  )
+}
+
+export default FoodDisplay
