c555f2c14fd0078db712e3bf06194ea64132d051
BewJSDK <bewjsdk41@gmail.com>
2024-09-30 19:18:47 +0700

    add comment to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,33 @@
-import Navbar from "./components/Navbar/Navbar";
-import styles from "./app.module.css";
-import Home from "./pages/Home/Home";
-import Footer from "./components/Footer/Footer";
 import { useState } from "react";
+
+import Home from "./pages/Home/Home";
+
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
 
+import styles from "./app.module.css";
+
+// Main App Component
 const App = () => {
 
+  // Show login popup state
   const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
-    {showLogin && <LoginPopup setShowLogin={setShowLogin}/>}
+      {/* Show login popup if true */}
+      {showLogin && <LoginPopup setShowLogin={setShowLogin}/>}
+      
+      {/* Main app */}
       <div className={styles.app}>
+        {/* Navbar */}
         <Navbar setShowLogin={setShowLogin}/>
+        {/* Home page */}
         <Home />
       </div>
       
+      {/* Footer section */}
       <Footer />
     </>
   );
